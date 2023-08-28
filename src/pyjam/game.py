31e9217fd00a5880fac24001df5821da6a3bc389
Tristan Slater <hello@trslater.ca>
2023-08-28 11:28:35 -0700

    Change to using scene instead of separate sprite lists

--- src/pyjam/game.py
+++ src/pyjam/game.py
@@ -14,20 +14,23 @@ class Game(arcade.Window):
 
         self.pixel_size = pixel_size
 
+        self.scene = None
+
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
 
-        self.player_list = arcade.SpriteList()
+        self.scene = arcade.Scene()
+        self.scene.add_sprite_list("Player")
 
         self.player_sprite = Player(self.pixel_size)
-        self.player_sprite.center_x = 400
-        self.player_sprite.center_y = 400
+        self.player_sprite.center_x = self.width//2
+        self.player_sprite.center_y = self.height//2
 
-        self.player_list.append(self.player_sprite)
+        self.scene.add_sprite("Player", self.player_sprite)
 
     def on_draw(self):
         """Render the screen."""
 
         self.clear()
         
-        self.player_list.draw(filter=NEAREST)
+        self.scene.draw(filter=NEAREST)
