b79e132391ff12a4c1600c29e300159e229348ca
Tristan Slater <hello@trslater.ca>
2023-08-28 11:08:08 -0700

    Set background to black

--- src/pyjam/game.py
+++ src/pyjam/game.py
@@ -3,11 +3,7 @@ import arcade
 
 class Game(arcade.Window):
     def __init__(self, screen_width, screen_height, screen_title):
-
-        # Call the parent class and set up the window
         super().__init__(screen_width, screen_height, screen_title)
-
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
