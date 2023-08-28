9803b9c073f10b66cd12da3068eed72764f970b7
Tristan Slater <hello@trslater.ca>
2023-08-28 12:03:11 -0700

    Add walls and player movement

--- src/pyjam/game.py
+++ src/pyjam/game.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import arcade
 from arcade.gl import NEAREST
 
@@ -21,12 +23,56 @@ class Game(arcade.Window):
 
         self.scene = arcade.Scene()
         self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
 
         self.player_sprite = Player(self.pixel_size)
         self.player_sprite.center_x = self.width//2
         self.player_sprite.center_y = self.height//2
 
+        for i in range(10):
+            wall = arcade.Sprite(
+                "assets/wall.png",
+                scale=self.pixel_size, image_width=16, image_height=16)
+            
+            wall.center_x = randint(0, self.width - 16)
+            wall.center_y = randint(0, self.height - 16)
+
+            self.scene.add_sprite("Walls", wall)
+
         self.scene.add_sprite("Player", self.player_sprite)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls"))
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 5
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -5
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -5
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 5
+    
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """Movement and game logic"""
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
     def on_draw(self):
         """Render the screen."""
